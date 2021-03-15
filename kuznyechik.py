0878fbf25fd83e9bdcd43f4828aef260601b66e6
Terry Jackson <58715371+jacksoninfosec@users.noreply.github.com>
2021-03-15 13:00:09 -0600

    Update kuznyechik.py

--- kuznyechik.py
+++ kuznyechik.py
@@ -187,11 +187,20 @@ def kuznyechik_decrypt(x, k):
 
 
 
-
+# plaintext
 PT = int('1122334455667700ffeeddccbbaa9988', 16)
+
+# key
 k = int('8899aabbccddeeff0011223344556677fedcba98765432100123456789abcdef', 16)
+
+# ciphertext
 CT = kuznyechik_encrypt(PT, k)
+print(hex(CT))
+
+# decrypted text
 DT = kuznyechik_decrypt(CT, k)
+
+# The plaintext should equal the decrypted text. 
 print(PT == DT)
 
 
